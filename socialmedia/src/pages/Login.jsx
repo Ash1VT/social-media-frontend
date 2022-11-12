c775b55a0cdc9b5e24df64027fe41f7586426301
Ash1VT <nikita.panasevitch@yandex.by>
2022-11-12 23:51:48 +0300

    added login page

---
+++ socialmedia/src/pages/Login.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import LoginForm from '../components/LoginForm';
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+
+  	return (
+		<div className="container">
+			<div className="form login">
+				<span className="title">Login</span>
+				<LoginForm/>
+				<div className="login-signup">
+					<span className="text">Not a member?</span>
+					<Link to='/register' className="text signup-link">Signup Now</Link>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Login;
